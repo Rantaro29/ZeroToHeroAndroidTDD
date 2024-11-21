08a4d16a1fcc3a0b9f9c768dd5e1520b2404dc0e
Rantaro29 <nazanian09@gmail.com>
2024-11-21 18:16:32 +0300

    test 10

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,35 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var clickable = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.removeButton.setOnClickListener{
+            binding.rootLayout.removeView(binding.titleTextView)
+            binding.removeButton.isEnabled = false
+            clickable = true
+        }
+
+        setContentView(binding.root)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("key", clickable)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState.getBoolean("key")){
+            binding.rootLayout.removeView(binding.titleTextView)
+            binding.removeButton.isEnabled = false
+        }
     }
 }
