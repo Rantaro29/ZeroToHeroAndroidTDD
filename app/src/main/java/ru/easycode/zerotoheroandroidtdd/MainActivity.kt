2b132ff792020f5263100b700584dec3f8e6f2a3
Rantaro29 <nazanian09@gmail.com>
2024-11-21 18:49:10 +0300

    test 12

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,31 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.incrementButton.setOnClickListener{
+            binding.countTextView.text = (binding.countTextView.text.toString().toInt() + 2).toString()
+
+            if(binding.countTextView.text.toString().toInt() == 4)
+                binding.incrementButton.isEnabled = false
+        }
+
+        setContentView(binding.root)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("key", binding.incrementButton.isEnabled)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        binding.incrementButton.isEnabled = savedInstanceState.getBoolean("key")
     }
 }
