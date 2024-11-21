1279fc0884d7e95538b681df0f243e3897066879
Rantaro29 <nazanian09@gmail.com>
2024-11-21 18:27:20 +0300

    test 11

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,18 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.incrementButton.setOnClickListener{
+            binding.countTextView.text = (binding.countTextView.text.toString().toInt() + 2).toString()
+        }
+
+        setContentView(binding.root)
     }
 }
